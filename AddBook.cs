c73712310214841e82b8560b87b67f548c12a858
afrancis269 <afrancis269@gmail.com>
2022-02-14 12:45:22 +0700

    First commit

---
+++ AddBook.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Milestone4
+{
+    public partial class AddBook : Form
+    {
+        public AddBook()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string bookName = BookNameTxb.Text;
+            string authorName = AuthorNameTxb.Text;
+            string publisherName = PublisherNameTxb.Text;
+            double price = double.Parse(PriceTxb.Text);
+            string genre = GenreTxb.Text;
+            int year = Int32.Parse(GenreTxb.Text);
+            int quant = Int32.Parse(YearTxb.Text);
+            Book item = new Book(bookName, authorName, publisherName, price, genre, year, quant);
+            Form1 frm1 = new Form1(item);
+            frm1.Show();
+            
+
+
+        }
+
+    }
+}
